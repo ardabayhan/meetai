59675029853183b8aee12d9db4d85298d680d721
Arda Bayhan <arda.bayhan@ogr.ebyu.edu.tr>
2025-12-02 00:01:07 +0300

    asd

--- src/modules/meetings/schemas.ts
+++ src/modules/meetings/schemas.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
 export const meetingsInsertSchema = z.object({
-    name: z.string().min(1, { message: "Name is required" }).optional(),
-    agentId: z.string().min(1, { message: "Agent is required" }).optional(),
+    name: z.string().min(1, { message: "Name is required" }),
+    agentId: z.string().min(1, { message: "Agent is required" }),
 });
 
 export const meetingsUpdateSchema = meetingsInsertSchema.extend({
     id: z.string().min(1, { message: "Id is required" }),
     status: z
     .enum(['upcoming', 'active', 'completed', 'processing', 'cancelled'])
-    .optional()
+    .optional(),
+    name: z.string().min(1, { message: "Name is required" }).optional(),
+    agentId: z.string().min(1, { message: "Agent is required" }).optional()
 });
