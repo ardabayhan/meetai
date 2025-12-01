f032cfcfccfdc9abc984437e881a2e4ceca06630
Arda Bayhan <arda.bayhan@ogr.ebyu.edu.tr>
2025-12-02 00:28:27 +0300

    ddd

--- src/modules/meetings/ui/components/chat-ui.tsx
+++ src/modules/meetings/ui/components/chat-ui.tsx
@@ -39,9 +39,7 @@ export const ChatUI = ({
     const [channel, setChannel] = useState<StreamChannel>();
     const client = useCreateChatClient({
         apiKey: process.env.NEXT_PUBLIC_STREAM_CHAT_API_KEY!,
-        tokenOrProvider: async () => {
-            return await generateChatToken();
-        },
+        tokenOrProvider: generateChatToken,
         userData: {
             id: userId,
             name: userName,
